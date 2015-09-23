0ff76b6f88eec60905adf089da415ecbef668125
joshuajhun <joshuajhun@gmail.com>
2015-09-23 13:09:37 -0600

    working on getting test to pass. meaning they aren't hard coded in

--- lib/district_repository.rb
+++ lib/district_repository.rb
@@ -3,45 +3,81 @@ require 'CSV'
 
 class DistrictRepository
 
-  # def initialize(#i want it to have all the data available)
-  #
-  # end
+   def initialize(path)#i want it to have all the data available)
+    @districts = from_csv(path)
+   end
 
-  def self.from_csv(path)
+  def from_csv(path)
     fullpath = File.join(path, "Students qualifying for free or reduced price lunch.csv")
-    rows = CSV.read(fullpath, headers: true, header_converters: :symbol)
-    binding.pry
+    contents = CSV.open fullpath, headers: true, header_converters: :symbol
+    districts = []
+    contents.each do |row|
+      districts << District.new(row)
+      # district_name = row[:location]
+      # binding.pry
 
-
-    # rows.each do |row|
-
-    #this will return a new dist repository instance(just the word new)
+    end
+   districts#this will return a new dist repository instance(just the word new)
   end
 
   def find_by_name(district_name)
-    District.new
+    @districts.detect do |district|
+        district.location == district_name
+    end
+    #we want to itterate through this array
+    #upon itterating through the array depending on what
+    #is passed through is what we will shovel into a
+    #new array
   end
 end
 
 class EconomicProfile
-  def free_or_reduced_lunch_in_year(year)
-    0.125
-  end
+  attr_reader :timeframe,
+              :data
+
+  def initialize
+    @timeframe = attributes[:timeframe]
+    @data = attributes[:data]
+
+    def free_or_reduced_lunch_in_year(year)
+      @districts.detect do |district|
+        district.timeframe == year
+        end
+    end
+
+
 end
 
 class District
+attr_reader :location,
+            :timeframe,
+            :data,
+            :poverty_level
 
-    def name
-    end
+  def initialize(attributes)
+    @location  = attributes[:location]
+    @timeframe = attributes[:timeframe]
+    @data      = attributes[:data]
+    @poverty   = attributes[:poverty_level]
+      #do this for the rest of the attributes for a disrict
+  end
 
-    def state_wide_testing
-    end
+  def name
+  end
 
-    def enrollment
-    end
+  def state_wide_testing
+  end
+
+  def enrollment
+  end
 
   def economic_profile
-    EconomicProfile.new
+     if @poverty == "Eligible for Free or Reduced Lunch"
+
+     end
   end
+
+
+
 
 end
--- test/district_repository_test.rb
+++ test/district_repository_test.rb
@@ -1,24 +1,26 @@
 require 'minitest/autorun'
-require 'minitest/pride'
+require 'minitest/emoji'
 require './lib/district_repository'
+require 'pry'
 
 
 class TestEconomicProfile < Minitest::Test
   def test_free_or_reduced_lunch_in_year
     path = File.expand_path("../data", __dir__)
-    repository = DistrictRepository.from_csv(path)
+    repository = DistrictRepository.new(path)
     #is a method I write that loads all of my files
     #and it will live in my DistrictRepository class
     district   = repository.find_by_name("ACADEMY 20")
+    # binding.pry
 
     assert_equal 0.125, district.economic_profile.free_or_reduced_lunch_in_year(2012)
   end
 end
 
-class TestDistric < Minitest::Test
-
-  def test_it_has_a_class
-    assert District
-  end
-
-end
+# class TestDistric < Minitest::Test
+#
+#   def test_it_has_a_class
+#     assert District
+#   end
+#
+# end
