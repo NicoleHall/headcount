4a43828583d5c470db7cd6f3b6072962f436fb48
Nicole Hall <nicolealexandrahall@gmail.com>
2015-09-26 14:06:17 -0600

    Read district info from the real file

--- lib/district_repository.rb
+++ lib/district_repository.rb
@@ -1,10 +1,19 @@
 require "district"
+require "csv_reader"
 
 class DistrictRepository
   def initialize(path)
+    data_file = File.open(File.join(path, "Students qualifying for free or reduced price lunch.csv"))
+    @csv_reader = CsvReader.new(data_file)
   end
 
   def find_by_name(name)
-    District.new(name)
+    if csv_reader.location_exists?(name)
+      District.new(name)
+    end
   end
+
+  protected
+
+  attr_reader :csv_reader
 end
--- test/district_repository_test.rb
+++ test/district_repository_test.rb
@@ -7,11 +7,10 @@ require 'district_repository'
 class DistrictRespositoryTest < Minitest::Test
   def test_find_by_name_returns_a_district_with_that_name
     assert_equal "ACADEMY 20", district_repository.find_by_name("ACADEMY 20").name
-    assert_equal "ADAMS COUNTY", district_repository.find_by_name("ADAMS COUNTY").name
+    assert_equal "ADAMS COUNTY 14", district_repository.find_by_name("ADAMS COUNTY 14").name
   end
 
   def test_find_by_name_returns_nil_for_a_nonexistent_name
-    skip "We're going deeper!"
     assert_nil district_repository.find_by_name("wibble")
   end
 
